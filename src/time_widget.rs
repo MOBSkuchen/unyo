2ce605a5df51b67afa31ead4454fd3020fc6d00d
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2025-04-23 17:26:14 +0200

    Actually render some stuff

--- src/time_widget.rs
+++ src/time_widget.rs
@@ -2,6 +2,7 @@ use chrono::{Datelike, Weekday};
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use crate::display::WIDGET_COLOR;
+use crate::fraction;
 use crate::ui_renderer::{Drawable, UIContext, UIHelper, USize, EDGE_PADDING};
 
 pub struct TimeWidget {
@@ -10,7 +11,7 @@ pub struct TimeWidget {
 
 impl TimeWidget {
     pub fn new(screen_size: &USize) -> Self {
-        Self { position: screen_size.scale(0.4).to_rect(EDGE_PADDING(), EDGE_PADDING()) }
+        Self { position: screen_size.scale_1_2(fraction(5, 9), 0.5).to_rect(EDGE_PADDING(), EDGE_PADDING()) }
     }
     
     fn get_time_strs() -> (String, String) {
@@ -40,13 +41,20 @@ impl TimeWidget {
             }
         }.to_string();
         
-        (format!("{weekday} der {}", now.format("%d.%m.%Y")), now.format("%H:%M:%S").to_string())
+        (format!("{weekday} {}", now.format("%d.%m.%Y")), now.format("%H:%M:%S").to_string())
     }
 }
 
 impl Drawable for TimeWidget {
-    fn draw(&self, ctx: &mut UIContext, texture_creator: &UIHelper) {
+    fn draw(&self, ctx: &mut UIContext, uihelper: &UIHelper) {
+        let xp = self.position.x + 2 * EDGE_PADDING();
+        let jb_large_l_size = uihelper.font_owner.jb_large_l.char_dim();
+        
         let (date, time) = Self::get_time_strs();
+        
         ctx.draw_rect(self.position, WIDGET_COLOR);
+
+        let (x, y) = ctx.draw_text(xp, self.position.y + 2 * EDGE_PADDING(), &uihelper.font_owner.jb_large_l, time.as_str(), Color::WHITE, uihelper);
+        let (x, y) = ctx.draw_text(xp, y + 2 * jb_large_l_size.one() as i32, &uihelper.font_owner.jb_large_s, date.as_str(), Color::WHITE, uihelper);
     }
 }
