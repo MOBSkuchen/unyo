d980737c07edc3985ac989af846ea7f9264150a8
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2025-04-23 19:28:26 +0200

    Attach wifi connection update scheduler

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use crate::display::video_main;
+use crate::threads::start_wifi_con_update_thread;
 
 mod display;
 mod ui_renderer;
@@ -6,11 +7,14 @@ mod api;
 mod errors;
 mod weather_widget;
 mod time_widget;
+mod wifi_api;
+mod threads;
 
 pub(crate) const fn fraction(a: i32, b: i32) -> f32 {
     a as f32 / b as f32
 }
 
 fn main() {
+    start_wifi_con_update_thread();
     video_main().expect("FAILED");
 }
