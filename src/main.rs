7f7183be2d4ad6ce3c27f9ed5680424e3bc55400
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2025-04-23 17:03:59 +0200

    Fraction function and time widget

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ mod ui_renderer;
 mod api;
 mod errors;
 mod weather_widget;
+mod time_widget;
+
+pub(crate) const fn fraction(a: i32, b: i32) -> f32 {
+    a as f32 / b as f32
+}
 
 fn main() {
     video_main().expect("FAILED");
