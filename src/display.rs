fe758ebb2c1fe902f89e41be83cf375b7133e253
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2025-04-23 17:04:51 +0200

    Change drawing procedure

--- src/display.rs
+++ src/display.rs
@@ -3,7 +3,8 @@ use std::time::{Duration, Instant};
 use sdl2::event::Event;
 use sdl2::pixels::Color;
 use crate::api::WeatherInfo;
-use crate::ui_renderer::{init, surface_to_texture, AvailableFonts, Font, FontSize, UIContext, UIHelper};
+use crate::time_widget::TimeWidget;
+use crate::ui_renderer::{init, UIContext, UIHelper};
 
 pub const BACKGROUND_COLOR: Color = Color::RGB(35, 34, 34);
 pub const WIDGET_COLOR: Color = Color::RGB(58, 57, 57);
@@ -15,14 +16,13 @@ pub fn video_main() -> Result<(), String> {
     let texture_creator = canvas.texture_creator();
     let uihelper = UIHelper::new(&texture_creator);
     
-    let jetbrains = Font::load(AvailableFonts::JetbrainsMono, FontSize::SuperLarge.to_real_size());
-    
     let mut ui = UIContext::new(canvas).unwrap();
-    ui.clear(BACKGROUND_COLOR);
+    let ui_size = ui.size();
 
     let exit_time = Instant::now() + Duration::from_secs(3600);
 
-    let weather_widget = WeatherInfo::auto_construct_widget(ui.size());
+    let weather_widget = WeatherInfo::auto_construct_widget(&ui_size);
+    let time_widget = TimeWidget::new(&ui_size);
 
     'running: loop {
         if Instant::now() > exit_time {
@@ -34,14 +34,9 @@ pub fn video_main() -> Result<(), String> {
                 break 'running;
             }
         }
-
-        let now = chrono::Local::now();
-        let time_string = now.format("%H:%M:%S").to_string();
-        let (txt_surface, size) = jetbrains.write_text(time_string.as_str(), Color::BLUE);
-        let texture = surface_to_texture(&texture_creator, txt_surface);
-        let center = ui.center_rect(size.one(), size.two());
+        
         ui.clear(BACKGROUND_COLOR);
-        ui.draw_texture(texture, center);
+        ui.draw(&time_widget, &uihelper);
         ui.draw(&weather_widget, &uihelper);
         ui.render();
         
