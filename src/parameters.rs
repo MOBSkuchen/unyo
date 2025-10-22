9aff0b49812a464e9689f596f2f98b140d0e1211
MOBSkuchen <mobskuchen@gmail.com>
2025-10-22 22:46:53 +0200

    add a trim

--- src/parameters.rs
+++ src/parameters.rs
@@ -4,5 +4,5 @@ pub const P_DEVICE_NAME: &str = "/home/jasper/parameters/BLUETOOTH_DEVICE_NAME";
 
 #[inline]
 pub fn load_device_name_or_default() -> String {
-    fs::read_to_string(P_DEVICE_NAME).unwrap_or(String::from("Raspi Audio Player"))
+    fs::read_to_string(P_DEVICE_NAME).map(|s| s.trim().to_string()).unwrap_or(String::from("Raspi Audio Player"))
 }
