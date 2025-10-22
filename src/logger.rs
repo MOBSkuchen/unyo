7b6a1c7b70b46f4c7d32adc47b468780fc77276d
MOBSkuchen <mobskuchen@gmail.com>
2025-10-23 00:23:52 +0200

    Add auto flushing

--- src/logger.rs
+++ src/logger.rs
@@ -17,24 +17,32 @@ pub static _LOGFILE_HANDLE: LazyLock<Mutex<File>> =
     LazyLock::new(|| { Mutex::from(OpenOptions::new().append(true).create(true).open(get_logfile_path()).unwrap()) });
 
 #[inline]
-pub fn _log(s: &str) {
+pub(crate) fn _log(s: &str) {
     (*_LOGFILE_HANDLE.lock().unwrap()).write_all(s.as_bytes()).unwrap();
+}
+
+#[inline]
+pub(crate) fn _log_flush() {
+    (*_LOGFILE_HANDLE.lock().unwrap()).flush().unwrap();
 }
 
 #[macro_export]
 macro_rules! log {
     ($($arg:tt)*) => {{
         $crate::logger::_log(format!($($arg)*).as_str());
+        $crate::logger::_log_flush();
     }};
 }
 
 #[macro_export]
 macro_rules! logln {
     () => {
-        $crate::logger::_log("\n")
+        $crate::logger::_log("\n");
+        $crate::logger::_log_flush();
     };
     ($($arg:tt)*) => {{
         $crate::logger::_log(format!($($arg)*).as_str());
-        $crate::logger::_log("\n")
+        $crate::logger::_log("\n");
+        $crate::logger::_log_flush();
     }};
 }
