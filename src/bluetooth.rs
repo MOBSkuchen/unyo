bc6eca4f801fa51118fb8561e33ab32a3ed7768f
MOBSkuchen <mobskuchen@gmail.com>
2025-10-22 23:35:24 +0200

    Name can not be written to. Just noticed that

--- src/bluetooth.rs
+++ src/bluetooth.rs
@@ -102,15 +102,6 @@ pub async fn set_bluetooth_device_name(new_name: &str) -> Result<(), zbus::Error
             Value::new(new_name),
         )
     ).await?;
-
-    adapter_proxy.call_method(
-        "Set",
-        &(
-            "org.bluez.Adapter1",
-            "Name",
-            Value::new(new_name),
-        )
-    ).await?;
     Ok(())
 }
 
